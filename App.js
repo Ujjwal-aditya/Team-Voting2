15a592363363d970ad815a52aeb374ebed011b58
ujjwal-aditya <rashmiray4172@gmail.com>
2022-05-23 12:02:10 +0530

    app.js and config.js edited

--- App.js
+++ App.js
@@ -6,6 +6,7 @@ import Constants from 'expo-constants';
 
 // You can import from local files
 import HomeScreen from './screens/HomeScreen';
+import VoteGivenScreen from './screens/VoteGivenScreen';
 
 export default class App extends React.Component {
   render() {
@@ -18,7 +19,8 @@ export default class App extends React.Component {
 }
 
 var AppNavigator = createSwitchNavigator({
-  HomeScreen: HomeScreen
+  HomeScreen: HomeScreen,
+  VoteGivenScreen : VoteGivenScreen
 });
 
 const AppContainer = createAppContainer(AppNavigator);
--- config.js
+++ config.js
@@ -2,7 +2,16 @@ import firebase from 'firebase';
 
 // add SDK Firebase
 var firebaseConfig = {
-
+   
+        apiKey: "AIzaSyDh0Y33arq4ms9WG8anAOKCTpk1zU3PPDs",
+        authDomain: "voting-app-proc-58.firebaseapp.com",
+        databaseURL: "https://voting-app-proc-58-default-rtdb.firebaseio.com",
+        projectId: "voting-app-proc-58",
+        storageBucket: "voting-app-proc-58.appspot.com",
+        messagingSenderId: "222486131067",
+        appId: "1:222486131067:web:6edf4544a729cd6cf40dde"
+      
+      
 };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
